3d790ee491a88784af3cb2d11dad6db2e3b0f59c
kellykaufman <kellykaufman12@gmail.com>
2022-11-04 18:08:06 -0700

    Adds happy/sad path to users controllers create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(name: params[:name], password:params[:password], password_confirmation: params[:password_confirmation])
+
+    # happy/sad paths
+    if user.save
+      render json: {message: "User created successfully!"}, status: :created
+    else
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+    end
   end
 
 end

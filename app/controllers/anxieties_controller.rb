cc785b830ddef2517dd20b2f92580f232ae164d5
kellykaufman <kellykaufman12@gmail.com>
2022-11-30 18:25:28 -0800

    fixed anxiety typo in update method & corrected anxiety variable in update method

--- app/controllers/anxieties_controller.rb
+++ app/controllers/anxieties_controller.rb
@@ -27,16 +27,15 @@ class AnxietiesController < ApplicationController
   end
 
 def update
-anxiety = Anxiety.find_by(user: params[:id])
+anxiety = current_user.anxieties.find_by(id: params[:id])
 
 life_theme = params[:life_theme] || anxiety.life_theme
-intrusive_thought_or_feeling = params[:intrusive_thought_or_feeling] || an
-xiety.intrusive_thought_or_feeling
+intrusive_thought_or_feeling = params[:intrusive_thought_or_feeling] || anxiety.intrusive_thought_or_feeling
 obsessional_theme = params[:obsessional_theme] || anxiety.obsessional_them
 
 opposite_thought = params[:opposite_thought] || anxiety.opposite_thought
 opposite_action = params[:opposite_action] || anxiety.opposite_action
-timer = params[:timer] || timer.anxiety 
+timer = params[:timer] || anxiety.timer
 progress = params[:progress] || anxiety.progress
 anxiety.user_id = current_user.id || anxiety.user_id
 end
